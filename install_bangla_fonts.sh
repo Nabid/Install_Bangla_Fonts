c4ed6a52b2abe93771ad1a591e26e1f9eba6e126
Nabid <mail.prome@gmail.com>
2015-01-26 21:20:57 +0600

    updating script - change directory name

--- install_bangla_fonts.sh
+++ install_bangla_fonts.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-find ./ -type f -not -name "*.sh" -print -exec sudo cp -u '{}' /usr/share/fonts/ \;
+wget https://github.com/Nabid/Install_Bangla_Fonts/archive/master.zip -O Install_Bangla_Fonts.zip && unzip -x Install_Bangla_Fonts.zip && cd Install_Bangla_Fonts-master/ && chmod +x bangla_fonts.sh && ./bangla_fonts.sh  && cd .. && rm -r Install_Bangla_Fonts-master/ && echo 'Above bangla fonts has been successfully installed.' 
